53cb2fae3108594c8090b8ed695c1d1cff90bdc9
Tesmen <tezmo@mail.ru>
2015-08-26 23:47:05 +0300

    twig works

--- web/index.php
+++ web/index.php
@@ -1,14 +1,24 @@
 <?php
 
 // web/index.php
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/{name}', $response);
+$app->get('/{name}', function ($name) use ($app) {
+    return 'Hello ' . $app->escape($name);
+});
 
-$response = function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
-};
 $app->run();
+
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('../templates');
+$twig = new Twig_Environment($loader, [
+//    'cache' => '../cache',
+]);
+$template = $twig->loadTemplate('index.html.twig');
+echo $template->render([
+
+    'title' => 'hello',
+]);

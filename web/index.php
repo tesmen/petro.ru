e85070ea4897789939d7c38b9efd1632d97114eb
Tesmen <tezmo@mail.ru>
2015-08-27 00:28:47 +0300

    2 routes

--- web/index.php
+++ web/index.php
@@ -1,16 +1,27 @@
 <?php
 
-// web/index.php
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/{name}', function ($name) use ($app) {
-    return 'Hello ' . $app->escape($name);
+/**
+ * default route
+ */
+$app->get('/', function () use ($app) {
+    return 'Hello ';
+});
+
+/**
+ * main route
+ */
+$app->get('/{name}/{sec}', function ( $sec,$name) {
+    return 'Hello ' . $name . $sec;
 });
 
 $app->run();
+
+
 
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('../templates');
@@ -19,6 +30,6 @@ $twig = new Twig_Environment($loader, [
 ]);
 $template = $twig->loadTemplate('index.html.twig');
 echo $template->render([
-
     'title' => 'hello',
 ]);
+

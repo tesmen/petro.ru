5d7844d1bb89fe51856a82278872f9ec24736010
Tesmen <tezmo@mail.ru>
2015-08-28 00:18:37 +0300

    2nd day

--- web/index.php
+++ web/index.php
@@ -5,22 +5,22 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+
 /**
  * default route
  */
 $app->get('/', function () use ($app) {
-    return 'Hello ';
+    return "";;
 });
 
 /**
  * main route
  */
-$app->get('/{name}/{sec}', function ( $sec,$name) {
+$app->get('/{name}/{sec}', function ($sec, $name) {
     return 'Hello ' . $name . $sec;
 });
 
 $app->run();
-
 
 
 Twig_Autoloader::register();
@@ -28,8 +28,9 @@ $loader = new Twig_Loader_Filesystem('../templates');
 $twig = new Twig_Environment($loader, [
 //    'cache' => '../cache',
 ]);
+
 $template = $twig->loadTemplate('index.html.twig');
 echo $template->render([
-    'title' => 'hello',
+
 ]);
 

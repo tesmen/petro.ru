344ee4b02c05e1344cb6c27e76f74b2cc0736d4e
Tesmen <tezmo@mail.ru>
2015-08-30 17:23:41 +0300

    class actions

--- web/index.php
+++ web/index.php
@@ -5,23 +5,43 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+class PetroBalt
+{
+    private $silexApp;
 
-/**
- * default route
- */
-$app->get('/', function () use ($app) {
-    return "";;
-});
+    public function __construct($silexApp){
+        $this->silexApp= $silexApp;
 
-/**
- * main route
- */
-$app->get('/{name}/{sec}', function ($sec, $name) {
-    return 'Hello ' . $name . $sec;
-});
+        $this->loadRoutes();
 
-$app->run();
+        $this->silexApp->run();
+    }
 
+    public function init(){
+
+    }
+
+    public function loadRoutes(){
+        $app = $this->silexApp;
+
+        $this->silexApp->get('/', function () use ($app) {
+            return "/";
+        });
+
+        $this->silexApp->get('/{name}/{sec}', function ($sec, $name) {
+            return '{name}/{sec} ' . $name . $sec;
+        });
+    }
+
+    public function mainAction(){
+
+    }
+}
+
+$site = new PetroBalt($app);
+
+?>
+<!--
 
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('../templates');
@@ -33,4 +53,3 @@ $template = $twig->loadTemplate('index.html.twig');
 echo $template->render([
 
 ]);
-

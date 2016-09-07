716fc81bf6a2fea78230b7dd6afaa429c9f618ce
tesmen <tezmo@mail.ru>
2016-09-08 02:18:01 +0300

    news #1

--- web/js/app.js
+++ web/js/app.js
@@ -1,5 +1,6 @@
 $(function() {
-    $('.image_pop').on('click', function() {
+    $('.image_pop').on('click', function(e) {
+        e.preventDefault();
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
         $('#imagemodal').modal('show');
     });
